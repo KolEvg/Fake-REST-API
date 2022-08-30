9c338d6180ad376d7602a3d2469e94d78c5ed0e1
SkiPPy43 <skippy4343@yandex.ru>
2022-08-30 19:29:45 +0300

    grid

--- src/components/User.js
+++ src/components/User.js
@@ -9,12 +9,12 @@ const User = ({id, email, name, website, address, onDelete}) => {
 
     return (
         <div className='list'>
-            <span>{id}</span>
-            <span>{name}</span>
-            <span>{email}</span>
-            <span>{address}</span>
-            <span>{website}</span>
-            <span>
+            <span className='id'>{id}</span>
+            <span className='name'>{name}</span>
+            <span className='email'>{email}</span>
+            <span className='address'>{address}</span>
+            <span className='website'>{website}</span>
+            <span className='buttons'>
                 <button>edit</button>
                 <button onClick={handleDelete}>delete</button>
             </span>
