82f937d6a5c70aa51a3a9df99822f9fef00273b8
SkiPPy43 <skippy4343@yandex.ru>
2023-01-10 23:43:16 +0300

    fix button

--- src/components/User.js
+++ src/components/User.js
@@ -15,7 +15,7 @@ const User = ({ id, email, name, website, address, onDelete }) => {
       <span className='address'>{address}</span>
       <span className='website'>{website}</span>
       <span className='buttons'>
-        <button>Edit</button>
+        <button>Edit Users</button>
         <button onClick={handleDelete}>Delete</button>
       </span>
     </div>
